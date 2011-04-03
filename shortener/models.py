3475f21a3bcceafb447eccf6cc2c196f662a1964
xiao_haozi <xiao_haozi@mutaku.com>
2011-04-03 01:30:25 -0400

    models updated

--- shortener/models.py
+++ shortener/models.py
@@ -1,25 +1,12 @@
 from django.db import models
 
-"""
-old database setup
-+----------+-------------+------+-----+-------------------+----------------+
-| Field    | Type        | Null | Key | Default           | Extra          |
-+----------+-------------+------+-----+-------------------+----------------+
-| id       | int(15)     | NO   | PRI | NULL              | auto_increment | 
-| longurl  | blob        | YES  |     | NULL              |                | 
-| hash_url | varchar(60) | YES  |     | NULL              |                | 
-| shorturl | varchar(15) | YES  |     | NULL              |                | 
-| hits     | bigint(20)  | YES  |     | NULL              |                | 
-| date     | timestamp   | NO   |     | CURRENT_TIMESTAMP |                | 
-+----------+-------------+------+-----+-------------------+----------------+
-"""
 
 class urls(models.Model):
 	"database layout for storing the urls and statistics"
 	longurl = models.URLField("Original URL",max_length=2000,verify_exists=True)
 	hashurl = models.CharField("Hash of original URL",max_length=60)
-	shorturl = models.CharField("Shortened URL",max_length=15)
-	hits = models.PositiveIntegerField("Number of visits")
+	shorturl = models.CharField("Shortened URL",max_length=15,null=True)
+	hits = models.PositiveIntegerField("Number of visits",default=1)
 	created = models.DateTimeField("Created timestamp",auto_now=True)
 	lastvisit = models.DateTimeField("Last visit timestamp",auto_now_add=True)
-	cookie = models.CharField("Associated cookie",max_length=10)
+	cookie = models.CharField("Associated cookie",max_length=10,null=True)
