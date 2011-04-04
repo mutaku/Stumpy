2f054580703d329efb312396d80b698e6b7465a4
xiao_haozi <xiao_haozi@mutaku.com>
2011-04-04 00:24:16 -0400

    fixed up views error and added a 404 response on grabbing urls

--- shortener/views.py
+++ shortener/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django import forms
 from shortener.models import url
+from django.utils.encoding import smart_str
+from django.shortcuts import get_object_or_404
 
 def index(request):
 	stumps = url.objects.all()
@@ -8,11 +10,16 @@ def index(request):
 
 
 def detail(request,short):
-	thisurl = url.objects.get(shorturl=short)
+	thisurl = get_object_or_404(url,shorturl=short)
 	fullurl = thisurl.longurl	
 	thisurl.hits += 1
 	thisurl.save()
 	return HttpResponse("You use short link %s which is translating to %s" % (short,fullurl))
 
-def submit(request,url):
-	return HttpResponse("Adding link %s" % url)
+def submit(request,stump):
+	b = url(longurl=smart_str(stump))
+	b.save()
+	thisid = b.id	
+	short = url.objects.get(id=thisid).shorturl
+	long = url.objects.get(id=thisid).longurl 
+	return HttpResponse("Added %s ==> %s" % (long,short))
