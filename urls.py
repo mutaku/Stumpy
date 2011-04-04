7b5bd77c5d0cf62303a238f831210e4becbae975
xiao_haozi <xiao_haozi@mutaku.com>
2011-04-04 00:24:51 -0400

    fixed admin parsing problem -partially

--- urls.py
+++ urls.py
@@ -8,16 +8,17 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Stumpy.views.home', name='home'),
     # url(r'^Stumpy/', include('Stumpy.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
     
     # show the index from /
     url(r'^$', 'shortener.views.index'),
     # get a url for redirection /shorty
     url(r'^(?P<short>\w+)/$', 'shortener.views.detail'),
     # send a url to be shortened from /url/someencodedurl
-    url(r'^url/(?P<url>\S+)/$', 'shortener.views.submit'),
+    url(r'^url/(?P<stump>\S+)/$', 'shortener.views.submit'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
