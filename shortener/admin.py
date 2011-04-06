df64e0d4d1058771352913fcab31606c81056d42
xiao_haozi <xiao_haozi@mutaku.com>
2011-04-05 21:35:26 -0400

    added hashurl functionality to the admin interface

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,9 +1,22 @@
 from shortener.models import stumps
 from django.contrib import admin
+import hashlib
 
 class stumpsAdmin(admin.ModelAdmin):
 	list_display = ('long_to_less_long','shorturl','hashurl','created','hits','cookie')
 	search_fields = ['longurl','cookie']	
 	date_hierarchy = 'created'
 
+	def save_model(self,request,obj,form,change):
+		obj.hashurl = hashlib.sha1(obj.longurl).hexdigest()	
+		obj.save()
+
 admin.site.register(stumps,stumpsAdmin)
+
+
+
+
+
+
+
+
