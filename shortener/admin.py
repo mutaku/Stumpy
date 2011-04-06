7f0777acd26cfef9e1f76981c8abf36aab5ffa22
xiao_haozi <xiao_haozi@mutaku.com>
2011-04-06 16:26:05 -0400

    added username->cookie function to the admin interface submission

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,6 +1,7 @@
 from shortener.models import stumps
 from django.contrib import admin
 import hashlib
+from django.utils.encoding import smart_str
 
 class stumpsAdmin(admin.ModelAdmin):
 	list_display = ('long_to_less_long','shorturl','hashurl','created','hits','cookie')
@@ -9,6 +10,7 @@ class stumpsAdmin(admin.ModelAdmin):
 
 	def save_model(self,request,obj,form,change):
 		obj.hashurl = hashlib.sha1(obj.longurl).hexdigest()	
+		obj.cookie = smart_str(request.user)
 		obj.save()
 
 admin.site.register(stumps,stumpsAdmin)
