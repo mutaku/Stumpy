cc4193462c3b85896abb2bb5d256a7cf554a6dc4
xiao_haozi <xiao_haozi@mutaku.com>
2011-04-18 16:43:53 -0400

    rewrote the keygen script

--- gimme_key.py
+++ gimme_key.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 import string
-from random import choice
+from random import sample
 
-key = ""
-for x in [choice(string.letters+string.digits+string.punctuation) for y in range (50)]:
-	key += x
 print
 print "Here is your key. Copy the following and use for SECRET_KEY in local_settings.py."
 print
-print key
-print
+print "Key ==> ",''.join(sample(string.letters+string.digits+string.punctuation,50))
 print
