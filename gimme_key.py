03d4c3c091f8582c6f76b7bac0e6991e1678cd7f
xiao_haozi <xiao_haozi@mutaku.com>
2011-04-18 21:35:38 -0400

    stripped out single and double quotations for variable assignment sanity

--- gimme_key.py
+++ gimme_key.py
@@ -2,8 +2,10 @@
 import string
 from random import sample
 
+AVAIL_CHARS = string.digits+string.letters+string.punctuation.replace("'","").replace('"',"")
+
 print
 print "Here is your key. Copy the following and use for SECRET_KEY in local_settings.py."
 print
-print "Key ==> ",''.join(sample(string.letters+string.digits+string.punctuation,50))
+print "Key ==> ",''.join(sample(AVAIL_CHARS,50))
 print
