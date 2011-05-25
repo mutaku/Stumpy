a92fe10890700d88e8a534785c0192e918ddc684
xiao_haozi <xiao_haozi@mutaku.com>
2011-05-25 17:06:48 -0400

    update

--- gimme_key.py
+++ gimme_key.py
@@ -7,5 +7,5 @@ AVAIL_CHARS = string.digits+string.letters+string.punctuation.replace("'","").re
 print
 print "Here is your key. Copy the following and use for SECRET_KEY in local_settings.py."
 print
-print "Key ==> ",''.join(sample(AVAIL_CHARS,50))
+print "Key ==> ","".join(sample(AVAIL_CHARS,50))
 print
